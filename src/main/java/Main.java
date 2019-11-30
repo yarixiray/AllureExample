fb7f9381e4bbfbe0e089ce5bd063575e6ba4d741
yarixiray <takemycarcompany@gmail.com>
2019-11-30 12:19:55 +0100

    Second update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi there !");
-        System.out.println("Wew fdsf");
+        System.out.println("One");
+        System.out.println("Two");
     }
 }
